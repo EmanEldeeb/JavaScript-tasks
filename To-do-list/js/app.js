8cc955b48dd3d3c54bc76bf872a0fecd420cc1a0
unknown <emaneldeeb1999@gmail.com>
2023-12-20 21:58:48 +0200

    add missed status

--- To-do-list/js/app.js
+++ To-do-list/js/app.js
@@ -1,15 +1,22 @@
 const addBtn=document.getElementById("add-btn");
 const userInput=document.getElementById("task");
+const taskDeadline=document.getElementById("task-date");
 const taskArea=document.getElementById("task-area");
-
+const today=new Date();
+let deadLine;
+//check every day
+const checkTime=1*24*60*60*1000
 function clearInput(){
     userInput.value=""
+    taskDeadline.value=''
 }
 
 addBtn.addEventListener("click",()=>{
+    
     const newRow=document.createElement("tr");
     const element=`<td>${userInput.value}</td>
     <td> <button class="status-btn btn btn-info">pendding</button> </td>
+    <td class="dead-line">${taskDeadline.value}</td>
     <td> <span class="delete"><i class="fa-regular fa-trash-can"></i></span></td>`
     newRow.innerHTML=element
     if(userInput.value){
@@ -33,3 +40,15 @@ taskArea.addEventListener("click",(event)=>{
     }
 
 })
+setInterval(()=>{
+    document.querySelectorAll(".dead-line").forEach(date=>{
+        if(new Date(date.textContent) <today){
+            const statusBtn=  date.closest("tr").querySelector("button")
+            statusBtn.textContent="missed"
+            statusBtn.classList.remove("btn-info")
+            statusBtn.classList.add("btn-danger")
+
+        }
+    })
+},20000 ) //should use checkTime to cheak every day to be implemented
+
